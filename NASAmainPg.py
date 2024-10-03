ca1f73ee5ab32651eb2da5c5197916761724be06
Shumail-Fatima <139956990+Shumail-Fatima@users.noreply.github.com>
2024-10-03 22:33:27 +0500

    Delete NASAmainPg.py

--- NASAmainPg.py
+++
@@ -1,8 +0,0 @@
-print("Get Satellit Surface Reflectance Data")
-option = str(input("Location Name or Location Coordinates = "))
-if option == "location name":
-    import NASAplaceDisplay
-else:
-    import NASAcoordinateDisplay
-
-print("thank you for using")
